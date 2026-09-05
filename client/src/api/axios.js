d019f331d4d18cefd3fc9e146eed455cfb796fac
Atiba Dee 💥 <atiba.dar12345@gmail.com>
2026-09-05 17:22:18 +0500

    front end changes

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
  * TASK 5.1 - One axios instance for the whole app.
  */
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
