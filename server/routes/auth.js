28f4248b8e6fcccd5ec41726d38504e8c0ca294f
Atiba Dee 💥 <atiba.dar12345@gmail.com>
2026-09-06 01:22:27 +0500

    Change SameSite cookie attribute to 'lax'

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -13,7 +13,7 @@ const router = express.Router();
 const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "true",
+  sameSite: "lax",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
 };
 
